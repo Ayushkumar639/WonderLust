1a617eb12f9f5327498ec84614083673091ae313
Ayush <ayushjangid203@gmail.com>
2026-08-14 22:20:20 +0530

    Fixed image uploading issue

--- cloudConfig.js
+++ cloudConfig.js
@@ -12,7 +12,10 @@ const storage = new CloudinaryStorage({
     params: {
         folder: 'wanderlust_DEV',
         allowedFormats: ["png", "jpg", "jpeg"],
-        public_id: (req, file) => 'computed-filename-using-request',
+        public_id: (req, file) => {
+            let fileName = file.originalname.split('.')[0];
+            return `${fileName}-${Date.now()}`;
+        }
     },
 });
 
